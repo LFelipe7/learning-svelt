44718b6b11dfc21c1f31a11144d78225f9d2e381
filipemarch <filipe.marchesini@gmail.com>
2023-09-19 17:46:38 -0300

    💄 Added green arrow to ClassificationCard

--- src/routes/ClassificationCard.svelte
+++ src/routes/ClassificationCard.svelte
@@ -1,13 +1,19 @@
 <script>
   import DiCoffeescript from "svelte-icons/di/DiCoffeescript.svelte";
+  import FaArrowUp from "svelte-icons/fa/FaArrowUp.svelte";
   export let coffeeClassification = "Mole";
 </script>
 
 <div class="bg-stone-50 w-64 flex flex-col pt-5 gap-4 rounded-md">
   <div
-    class="icon w-20 bg-white self-center rounded-full border-solid border-stone-200 border-2"
+    class="relative icon w-20 bg-white self-center border-solid border-stone-200 border-2 rounded-full"
   >
-    <DiCoffeescript />
+    <div class="absolute w-5 z-10 right-2 top-4" style="color: #3FA152">
+      <FaArrowUp />
+    </div>
+    <div class="relative" style="color: #444444">
+      <DiCoffeescript />
+    </div>
   </div>
   <div class="text-center text-2xl font-semibold place-content-center">
     {coffeeClassification}
