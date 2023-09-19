a1a76cdfea515677c07fd248e063e2e63a86d4cf
filipemarch <filipe.marchesini@gmail.com>
2023-09-19 16:41:01 -0300

    🔥 Removed unused code from `+page.svelte`

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,41 +22,11 @@
 </section>
 
 <style>
-	.profileImage {
-		height: 64px;
-		width: 64px;
-	}
-	/* p {
-		color: goldenrod;
-		font-family: 'Comic Sans MS', cursive;
-		font-size: 2em;
-		background-color: aqua;
-	} */
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
+  section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    flex: 0.6;
+  }
 </style>
